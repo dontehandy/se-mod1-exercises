a12cebcaabdba5b5bb90b1a242b6a547924994bd
Donte Handy <donte.handy@gmail.com>
2024-12-13 11:26:47 -0400

    completed hashes within arrays

--- lessons/nested_collections/Examples/hashes_within_arrays.rb
+++ lessons/nested_collections/Examples/hashes_within_arrays.rb
@@ -1,6 +1,31 @@
 food_feelings = [{:pizza => "tasty"}, {:calzone => "also tasty"}]
+#name or array assigned two hashes within an array
+#considered a nested array with 2 elements
+#each element is a hash with a key and value
+#the key is a symbol (:pizza) and the value is a string "tasty"
+#=> hash rocket for association
 
-# 1. what is food_feelings.count
-# 2. what is food_feelings.first.count
-# 3. how can I access the value "also tasty"
-# 4. how can I change the value “also tasty” to “super delicious”
+1. what is food_feelings.count - 2
+
+# - the count method returns the number of elements in an array
+# - the return would be 2 because there are 2 elements in the food_feelings array
+
+2. what is food_feelings.first.count - 1
+# - the first method returns the first element in an array
+# - the count method returns the number of elements in an array
+# - the return would be 1 because the first element in the food_feelings array has 1 element
+
+3. how can I access the value "also tasty" - food_feelings[1][:calzone] 
+# - "also tasty" is the value of the key :calzone in the second hash
+# - to access the value "also tasty", we would use food_feelings[1][:calzone] 
+# - too run a method on the value "also tasty", we would use food_feelings[1][:calzone].method
+
+4. how can I change the value “also tasty” to “super delicious” - food_feelings[1][:calzone] = "super delicious"
+# - to change the value "also tasty" to "super delicious", we would use food_feelings[1][:calzone] = "super delicious"
+# - [] indicate the element that we are changing in the array
+# - [:calzone] is the key that we are changing the value of
+# - = "super delicious" is the new value that we are assigning to the key :calzone
+
+
+
+
